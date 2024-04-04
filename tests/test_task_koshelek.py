1e82986d8ef40313c9cc594ade32566c0bdb3490
«Selezneva <petrukhina.marina2011@yandex.ru>
2024-04-04 17:46:59 +0300

    edit all tests

---
+++ tests/test_task_koshelek.py
@@ -0,0 +1,69 @@
+import pytest
+from playwright.sync_api import sync_playwright, expect
+
+@pytest.fixture(scope="module")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        yield browser
+        browser.close()
+
+
+def test_invalid_email_registration(browser):
+    page = browser.new_page()
+    page.goto("https://koshelek.ru")
+    page.get_by_text('Зарегистрироваться').click()
+    # Ввод некорректного email
+    page.get_by_label('Электронная почта').fill('invalid_email')
+    page.get_by_label('Пароль').fill('123')
+    expect(page.get_by_text("Формат e-mail: username@test.ru", exact=True)).to_be_visible()
+
+    
+def test_weak_password_registration(browser):
+    page = browser.new_page()
+    page.goto("https://koshelek.ru")
+    page.get_by_text('Зарегистрироваться').click()
+    # Ввод слабого пароля
+    page.get_by_label('Пароль').fill('123')
+    page.get_by_label('Электронная почта').fill('123')
+    expect(page.get_by_text("Пароль должен содержать минимум 8 символов", exact=True)).to_be_visible()
+
+    page.get_by_label('Пароль').blur()
+    page.get_by_label('Пароль').fill('12345678')
+    page.get_by_label('Электронная почта').fill('123')
+    expect(page.get_by_text("Пароль должен содержать от 8 до 64 символов, включая заглавные буквы и цифры", exact=True)).to_be_visible()
+
+
+def test_uncorrect_username_registration(browser):
+    page = browser.new_page()
+    page.goto("https://koshelek.ru")
+    page.get_by_text('Зарегистрироваться').click()
+    # Ввод короткого имени
+    page.get_by_label('Имя пользователя').fill('Iam')
+    page.get_by_label('Электронная почта').fill('1')
+    expect(page.get_by_text("Допустимые символы (от 6 до 32): a-z, 0-9, _. Имя должно начинаться с буквы", exact=True)).to_be_visible()
+    # Ввод длинного имени
+    page.get_by_label('Имя пользователя').blur()
+    page.get_by_label('Имя пользователя').fill('12345678ыуапкепкепывпапукпцукпцки')
+    expect(page.get_by_text("Превышен лимит символов: 32 максимум", exact=True)).to_be_visible()
+    # Начинается не с буквы
+    page.get_by_label('Имя пользователя').fill('45alisa')
+    page.get_by_label('Электронная почта').fill('2')
+    expect(page.get_by_text("Допустимые символы (от 6 до 32): a-z, 0-9, _. Имя должно начинаться с буквы", exact=True)).to_be_visible()
+    # Содержит невалидные символы
+    page.get_by_label('Имя пользователя').fill('alisa_25;')
+    page.get_by_label('Электронная почта').fill('3')
+    expect(page.get_by_text("Допустимые символы (от 6 до 32): a-z, 0-9, _. Имя должно начинаться с буквы", exact=True)).to_be_visible()
+    # Содержит кириллицу
+    page.get_by_label('Имя пользователя').fill('алиса_25;')
+    page.get_by_label('Электронная почта').fill('3')
+    expect(page.get_by_text("Допустимые символы (от 6 до 32): a-z, 0-9, _. Имя должно начинаться с буквы", exact=True)).to_be_visible()
+
+
+def test_missing_fields_registration(browser):
+    page = browser.new_page()
+    page.goto("https://koshelek.ru")
+    page.get_by_text('Зарегистрироваться').click()
+    # Оставление полей пустыми
+    page.get_by_text('Далее').click()
+    expect(page.get_by_text("Поле не заполнено", exact=True)).to_have_count(3)
